34c41f4af1e73d6ac39cb837fed73221beb5bebe
Chelsea David <cryan17@cnm.edu>
2018-08-04 14:18:21 -0600

    added basic html, filler <p> and cerberus img

---
+++ BootstrapChallenge.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="'width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<!-- Css link -->
+		<link rel="stylesheet" href="stylesheet/styles.css" type="text/css">
+		<title>Bootstrap Challenge</title>
+	</head>
+	<body>
+		<h1>It's LeviOsa not LevioSA</h1>
+		<p>
+			The wand chooses the wizard You sort of start thinking anythings possible if youve got enough nerve There are
+			all kinds of courage I think Ill just go and have some pudding Its wingardium leviOsa not leviosAH Give her
+			hell from us Peeves There are some things you cant share without ending up liking each other and knocking out
+			a twelvefoot mountain troll is one of them But you know happiness can be found even in the darkest of times if
+			one only remembers to turn on the light. It takes a great deal of bravery to stand up to our enemies but just
+			as much to stand up to our friends I think Ill just go and have some pudding But you know happiness can be
+			found even in the darkest of times if one only remembers to turn on the light It is our choices Harry that
+			show what we truly are far more than our abilities, It is our choices Harry that show what we truly are far
+			more than our abilities She needs to sort out her priorities Yer a wizard Harry Just because you have the
+			emotional range of a teaspoon doesnt mean we all have Now Im going to bed before either of you comes up with
+			another clever idea to get us killed Or worse expelled There are all kinds of courage Until the very end. Not
+			my daughter you bitch It is the unknown we fear when we look upon death and darkness nothing more After all
+			this time But you know happiness can be found even in the darkest of times if one only remembers to turn on
+			the light But you know happiness can be found even in the darkest of times if one only remembers to turn on
+			the light After all this time Its wingardium leviOsa not leviosAH Really Hagrid if you are holding out for
+			universal popularity Im afraid you will be in this cabin for a very long time There are some things you cant
+			share without ending up liking each other and knocking out a twelvefoot mountain troll is one of them.
+		</p>
+			<img src="images/cerberus.jpeg" alt="Cerberus">
+	</body>
+
+</html>
