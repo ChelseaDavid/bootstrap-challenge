a751b988029bea1243b98909f220b12541ce3fc5
Chelsea David <cryan17@cnm.edu>
2018-08-05 18:59:06 -0600

    completed bootstrap to the best of my ability

--- BootstrapChallenge.php
+++ BootstrapChallenge.php
@@ -10,27 +10,133 @@
 		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
-		<h1>It's LeviOsa not LevioSA</h1>
-		<p>
-			The wand chooses the wizard You sort of start thinking anythings possible if youve got enough nerve There are
-			all kinds of courage I think Ill just go and have some pudding Its wingardium leviOsa not leviosAH Give her
-			hell from us Peeves There are some things you cant share without ending up liking each other and knocking out
-			a twelvefoot mountain troll is one of them But you know happiness can be found even in the darkest of times if
-			one only remembers to turn on the light. It takes a great deal of bravery to stand up to our enemies but just
-			as much to stand up to our friends I think Ill just go and have some pudding But you know happiness can be
-			found even in the darkest of times if one only remembers to turn on the light It is our choices Harry that
-			show what we truly are far more than our abilities, It is our choices Harry that show what we truly are far
-			more than our abilities She needs to sort out her priorities Yer a wizard Harry Just because you have the
-			emotional range of a teaspoon doesnt mean we all have Now Im going to bed before either of you comes up with
-			another clever idea to get us killed Or worse expelled There are all kinds of courage Until the very end. Not
-			my daughter you bitch It is the unknown we fear when we look upon death and darkness nothing more After all
-			this time But you know happiness can be found even in the darkest of times if one only remembers to turn on
-			the light But you know happiness can be found even in the darkest of times if one only remembers to turn on
-			the light After all this time Its wingardium leviOsa not leviosAH Really Hagrid if you are holding out for
-			universal popularity Im afraid you will be in this cabin for a very long time There are some things you cant
-			share without ending up liking each other and knocking out a twelvefoot mountain troll is one of them.
-		</p>
-			<img src="images/cerberus.jpeg" alt="Cerberus">
-	</body>
+		<section class="navigation">
+					<div class="container">
+						<nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+							<div class="row">
+									<a class="navbar nav-brand ml-auto">Title</a>
+								</div>
+								<div class="col-12">
+									<button class="navbar-toggler text-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+										<span class="navbar-toggler-icon"></span>
+									</button>
+									<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+										<ul class="navbar-nav">
+											<li class="nav-item">
+												<a class="nav-link mr-auto" href="https://www.scholastic.com/kids/books/harry-potter/">link 1 </a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link mr-auto" href="https://en.wikipedia.org/wiki/Harry_Potter">link 2</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link mr-auto" href="https://www.pottermore.com">link 3</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link mr-auto" href="https://data.whicdn.com/images/79913354/large.jpg">link 4</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+		<div class="jumbotron">
+			<h1 class="display-4">It's LeviOsa not LeviosA</h1>
+			<p class="lead">Voldemort:"So all I have to do is lie?" Pinocchio:"Yep!"</p>
+			<hr class="my-4">
+			<p class="lead">
+				<a class="btn btn-primary btn-lg" href="https://www.pottermore.com/" role="button">Click if you dare!</a>
+			</p>
+			</div>
+		</section>
+		<section class="row container-fluid bg-dark text-white">
+			<div class="col-sm-4 text-center">
+				<img class="img-fluid" src="images/booYouWhore.gif" alt="voldemort"/>
+			</div>
+			<div class="col-sm-8 align-self-center">
+					<p>Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, <br>
+						Snape centaur portkey ghost Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby <br>
+						pear-tickle half-moon-glasses, Knight-Bus. Padfoot snargaluff seeker: Hagrid broomstick mischief<br>
+						managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods yew pumpkin juice<br>
+						phials Ravenclaw’s Diadem 10 galleons Thieves Downfall. Ministry-of-Magic mimubulus mimbletonia<br>
+						Pigwidgeon knut phoenix feather other minister Azkaban. Hedwig Daily Prophet treacle tart full-moon<br>
+						Ollivanders You-Know-Who cursed. Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden forest<br>
+						grindylows wool socks.</p>
+					</div>
+				</section>
 
+
+		<section class="container-fluid">
+			<div class="row justify-content-center">
+				<div class="card col-sm-3">
+					<img class="card-img-top rounded-circle" src="images/cerberus.jpeg" alt="Voldemort">
+					<div class="card-body">
+						<h4 class="card-title">Stupify!</h4>
+						<p class="card-text">
+							Really Hagrid if you are holding out for universal popularity Im afraid you
+							will be in this cabin for a very long time The things we lose always have a
+							way of coming back to us in the end It takes a great deal of bravery to stand
+							up to our enemies but just as much to stand up to our friends You sort of start
+							thinking anythings possible if youve got enough nerve I think Ill just go and have
+							some pudding It matters not what someone is born but what they grow to be Hermione
+							Granger Yer a wizard Harry Yer a wizard Harry.
+						</p>
+					</div>
+				</div>
+
+				<div class="card col-sm-3">
+					<img class="card-img-top rounded-circle" src="images/cerberus.jpeg" alt="Voldemort">
+					<div class="card-body">
+						<h4 class="card-title">Stupify!</h4>
+						<p class="card-text">
+							Really Hagrid if you are holding out for universal popularity Im afraid you
+							will be in this cabin for a very long time The things we lose always have a
+							way of coming back to us in the end It takes a great deal of bravery to stand
+							up to our enemies but just as much to stand up to our friends You sort of start
+							thinking anythings possible if youve got enough nerve I think Ill just go and have
+							some pudding It matters not what someone is born but what they grow to be Hermione
+							Granger Yer a wizard Harry Yer a wizard Harry.
+						</p>
+					</div>
+				</div>
+				<div class="card col-sm-3">
+					<img class="card-img-top rounded-circle" src="images/cerberus.jpeg" alt="Voldemort">
+					<div class="card-body">
+						<h4 class="card-title">Stupify!</h4>
+						<p class="card-text">
+							Really Hagrid if you are holding out for universal popularity Im afraid you
+							will be in this cabin for a very long time The things we lose always have a
+							way of coming back to us in the end It takes a great deal of bravery to stand
+							up to our enemies but just as much to stand up to our friends You sort of start
+							thinking anythings possible if youve got enough nerve I think Ill just go and have
+							some pudding It matters not what someone is born but what they grow to be Hermione
+							Granger Yer a wizard Harry Yer a wizard Harry.
+						</p>
+					</div>
+				</div>
+				<div class="card col-sm-3">
+					<img class="card-img-top rounded-circle" src="images/cerberus.jpeg" alt="Voldemort">
+					<div class="card-body">
+						<h4 class="card-title">Stupify!</h4>
+						<p class="card-text">
+
+							Really Hagrid if you are holding out for universal popularity Im afraid you
+							will be in this cabin for a very long time The things we lose always have a
+							way of coming back to us in the end It takes a great deal of bravery to stand
+							up to our enemies but just as much to stand up to our friends You sort of start
+							thinking anythings possible if youve got enough nerve I think Ill just go and have
+							some pudding It matters not what someone is born but what they grow to be Hermione
+							Granger Yer a wizard Harry Yer a wizard Harry.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="footer text-center bg-dark text-white">
+			<div class="row">
+				<div class="col-sm-8">
+				</div>
+			</div>
+				<div class="card-footer text-muted">
+					Wingardium LeviOsa!
+				</div>
+		</section>
 </html>
