980587f5cb1c80b65d00a18913ba5e08044a3cb5
Chelsea David <cryan17@cnm.edu>
2018-08-05 19:05:14 -0600

    completed Bootstrap Challenge

--- BootstrapChallenge.php
+++ BootstrapChallenge.php
@@ -5,8 +5,6 @@
 		<meta name="viewport" content="'width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<!-- Css link -->
-		<link rel="stylesheet" href="stylesheet/styles.css" type="text/css">
 		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
